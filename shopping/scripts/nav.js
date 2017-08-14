3951aaa9d052a472569e851fe5cfd398d5cd5c8c
zhanghaiyan10 <z30889115>
2017-08-14 14:16:14 +0800

    优化

--- shopping/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-//导航效果兼容ie6
-$(function(){
-    $("#item li:has(ul)").hover(function(){
-            $(this).children("ul").stop(true,true).slideDown(400);
-    }, function(){
-            $(this).children("ul").stop(true,true).slideUp("fast");
-    })
-});
