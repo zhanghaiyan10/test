9b7fbf7469f957345ea5bdf7ee26d645be417df6
zhanghaiyan10 <z30889115>
2017-08-15 13:59:20 +0800

    修改

--- shopping/scripts/detail.js
+++ shopping/scripts/detail.js
@@ -54,6 +54,27 @@ $(function(){
     })
 })
 
+
+//chageImg.js
+$(function(){
+    $(".img_list").livequery("click", function(event){
+        if(event.target.nodeName=="IMG"){
+            var imgValue=$(event.target).attr("src");
+            var key=imgValue.slice(0,-4);
+            $(event.target).parent().addClass("selected").siblings().removeClass("selected");
+            $(".jqzoom>img").attr("src", key+"_small.jpg");
+            $(".jqzoom>img").attr("jqImg", key+"_big.jpg");
+            $("#thickImg").attr("href",key+"_big.jpg");
+        }
+    });
+
+    $(".img_list li").hover(function(event){
+        $(this).addClass("selected");
+    }, function(){
+        $(this).removeClass("selected");
+    })
+
+})
 //tab.js
 $(function(){
     $(".tab_menu li").hover(function(){
--- shopping/scripts/index.js
+++ shopping/scripts/index.js
@@ -127,25 +127,6 @@ $(function(){
 
     })
 })
-//chageImg.js
-$(function(){
-    $(".img_list").livequery("click", function(event){
-        if(event.target.nodeName=="IMG"){
-            var imgValue=$(event.target).attr("src");
-            var key=imgValue.slice(0,-4);
-            $(event.target).parent().addClass("selected").siblings().removeClass("selected");
-            $(".jqzoom>img").attr("src", key+"_small.jpg");
-            $(".jqzoom>img").attr("jqImg", key+"_big.jpg");
-            $("#thickImg").attr("href",key+"_big.jpg");
-        }
-    });
 
-    $(".img_list li").hover(function(event){
-        $(this).addClass("selected");
-    }, function(){
-        $(this).removeClass("selected");
-    })
-
-})
 
 
